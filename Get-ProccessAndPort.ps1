4ac8b8ed5c8c722cdb9cf8c88023e0737c71e911
DPoirier1010 <43175085+DPoirier1010@users.noreply.github.com>
2021-04-28 11:28:57 -0400

    Create Get-ProccessAndPort.ps1

---
+++ Get-ProccessAndPort.ps1
@@ -0,0 +1,25 @@
+#dpoirier1010
+###Will give established or listening port and their respective application
+
+$ip = (Get-NetIPConfiguration).IPv4Address.IPAddress
+$programs = (Get-NetTCPConnection -LocalAddress $ip | Where-Object {($_.State -eq "Listen")}) ### Listen or Established
+
+
+Write-Host "Checking listening processes for local address  " $ip
+Write-Host " "
+Write-Host " ... "
+Write-Host " "
+foreach ($program in $programs) {
+    
+    $process = (Get-Process -PID $program.OwningProcess).Name
+    $localPort = $program.LocalPort
+    $remoteAddress = $program.RemoteAddress
+
+    Write-Host "Name --> " $process ";Port --> " $localPort ";Remote address --> " $remoteAddress
+
+    
+}
+Write-Host " "
+Write-Host " ... "
+Write-Host " "
+
