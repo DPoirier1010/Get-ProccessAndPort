2fd52c2d79a2dd7098a4d6a3a0eb04f12621a0be
DPoirier1010 <43175085+DPoirier1010@users.noreply.github.com>
2021-05-04 09:41:14 -0400

    Update Get-ProccessAndPort.ps1

--- Get-ProccessAndPort.ps1
+++ Get-ProccessAndPort.ps1
@@ -1,25 +1,43 @@
-#dpoirier1010
-###Will give established or listening port and their respective application
+    #dpoirier1010
+    ###Will give established or listening port and their respective application
 
-$ip = (Get-NetIPConfiguration).IPv4Address.IPAddress
-$programs = (Get-NetTCPConnection -LocalAddress $ip | Where-Object {($_.State -eq "Listen")}) ### Listen or Established
+    $ip = (Get-NetIPConfiguration).IPv4Address.IPAddress
+    $programs = (Get-NetTCPConnection -LocalAddress $ip | Where-Object {($_.State -eq "Established")}) ### Listen or Established
+    $programs2 = (Get-NetTCPConnection -LocalAddress $ip | Where-Object {($_.State -eq "Listen")}) ### Listen or Established
 
 
-Write-Host "Checking listening processes for local address  " $ip
-Write-Host " "
-Write-Host " ... "
-Write-Host " "
-foreach ($program in $programs) {
-    
-    $process = (Get-Process -PID $program.OwningProcess).Name
-    $localPort = $program.LocalPort
-    $remoteAddress = $program.RemoteAddress
+    Write-Host "Checking established processes for local address  " $ip " . . . "
+    Write-Host " "
+    Write-Host " ... "
+    Write-Host " "
+    foreach ($program in $programs) {
+        
+        $process = (Get-Process -PID $program.OwningProcess -ErrorAction SilentlyContinue).Name
+        $localPort = $program.LocalPort
+        $remoteAddress = $program.RemoteAddress
 
-    Write-Host "Name --> " $process ";Port --> " $localPort ";Remote address --> " $remoteAddress
+        Write-Host "Name --> " $process ";Port --> " $localPort ";Remote address --> " $remoteAddress -ErrorAction SilentlyContinue
 
-    
-}
-Write-Host " "
-Write-Host " ... "
-Write-Host " "
+        
+    }
+    Write-Host " "
+    Write-Host " ... "
+    Write-Host " "
+    Write-Host "Checking listening processes for local address  " $ip " . . . "
+    Write-Host " "
+    Write-Host " ... "
+    Write-Host " "
+    foreach ($program2 in $programs2) {
+        
+        $process = (Get-Process -PID $program2.OwningProcess -ErrorAction SilentlyContinue).Name
+        $localPort = $program2.LocalPort
+        $remoteAddress = $program2.RemoteAddress
+
+        Write-Host "Name --> " $process ";Port --> " $localPort ";Remote address --> " $remoteAddress -ErrorAction SilentlyContinue
+
+        
+    }
+    Write-Host " "
+    Write-Host " ... "
+    Write-Host " "
 
